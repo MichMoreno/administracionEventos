0af19ed68a313ecc564c41824509dc9fe0579301
MichMoreno <fmdominguezmoreno16@gmail.com>
2025-12-10 09:40:58 -0600

    se comento lo del run en administracioneventosapplication

--- src/main/java/com/ipn/mx/administracioneventos/AdministracionEventosApplication.java
+++ src/main/java/com/ipn/mx/administracioneventos/AdministracionEventosApplication.java
@@ -13,11 +13,11 @@ public class AdministracionEventosApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String texto = "Lorem ipsum dolor sit amet consectetur adipiscing elit tempor risus dui porttitor, blandit aptent vitae at torquent rhoncus eget ultrices lacinia nostra, cubilia sociis arcu hac nunc netus magna morbi dis varius. Hac porta a consequat volutpat bibendum habitant libero risus, faucibus dapibus dis tortor placerat ornare leo, vitae diam mauris tempus phasellus ante porttitor. Dui faucibus aliquet massa tristique semper cursus quisque dignissim himenaeos vehicula rutrum sapien pellentesque, penatibus natoque sollicitudin facilisi nec sed accumsan potenti mollis congue nullam.";
+        /*String texto = "Lorem ipsum dolor sit amet consectetur adipiscing elit tempor risus dui porttitor, blandit aptent vitae at torquent rhoncus eget ultrices lacinia nostra, cubilia sociis arcu hac nunc netus magna morbi dis varius. Hac porta a consequat volutpat bibendum habitant libero risus, faucibus dapibus dis tortor placerat ornare leo, vitae diam mauris tempus phasellus ante porttitor. Dui faucibus aliquet massa tristique semper cursus quisque dignissim himenaeos vehicula rutrum sapien pellentesque, penatibus natoque sollicitudin facilisi nec sed accumsan potenti mollis congue nullam.";
 
         String to="fmdominguezmoreno16@gmail.com";
         String subject = "Lorem ipsum. Correo enviado desde Spring Boot";
-        emailService.enviarCorreo(to, subject, texto);
+        emailService.enviarCorreo(to, subject, texto);*/
     }
 
 
